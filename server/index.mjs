068a739b45a9668acc5b43c393e9089bee5102b6
James Edmonds <james@deckchairlabs.com>
2021-06-15 17:01:23 +0930

    top-level await

--- server/index.mjs
+++ server/index.mjs
@@ -23,12 +23,12 @@ let viteDevServer
 
 if (isProduction) {
   await import(`${root}/dist/server/importer.js`)
-  await app.register(fastifyStatic, {
+  app.register(fastifyStatic, {
     root: `${root}/dist/client/assets`,
     prefix: '/assets'
   })
 } else {
-  await app.register(middie)
+  app.register(middie)
   viteDevServer = await vite.createServer({
     root,
     server: { middlewareMode: true },
@@ -38,47 +38,43 @@ if (isProduction) {
 
 const renderPage = createPageRender({ viteDevServer, isProduction, root })
 
-async function main() {
-  app.get('/favicon.ico', (_, reply) => {
-    reply.header('cache-control', 'public, max-age=86400')
-    reply.code(204)
-    reply.send('')
-  })
+app.get('/favicon.ico', (_, reply) => {
+  reply.header('cache-control', 'public, max-age=86400')
+  reply.code(204)
+  reply.send('')
+})
 
-  app.get('*', async (request, reply) => {
-    const renderContext = {
-      url: request.url,
-      isProduction,
-      pageProps: {
-        service: process.env.K_SERVICE,
-        revision: process.env.K_REVISION || 'dev',
-      }
+app.get('*', async (request, reply) => {
+  const renderContext = {
+    url: request.url,
+    isProduction,
+    pageProps: {
+      service: process.env.K_SERVICE,
+      revision: process.env.K_REVISION || 'dev',
     }
+  }
 
-    const result = await renderPage(renderContext)
+  const result = await renderPage(renderContext)
 
-    /**
-     * We only want to cache things that can be cached
-     */
-    const cacheableStatusCodes = [200, 203, 204, 206, 300, 301, 404, 405, 410, 414, 501]
-    if (cacheableStatusCodes.includes(result.statusCode)) {
-      reply.header('cache-control', 'public, max-age=900, stale-while-revalidate=3600, stale-if-error=86400')
-    }
+  /**
+   * We only want to cache things that can be cached
+   */
+  const cacheableStatusCodes = [200, 203, 204, 206, 300, 301, 404, 405, 410, 414, 501]
+  if (cacheableStatusCodes.includes(result.statusCode)) {
+    reply.header('cache-control', 'public, max-age=900, stale-while-revalidate=3600, stale-if-error=86400')
+  }
 
-    if (result.nothingRendered) {
-      reply.code(404).send(null)
-    } else {
-      reply
-        .code(result.statusCode)
-        .type('text/html')
-        .send(result.renderResult)
-    }
-  })
+  if (result.nothingRendered) {
+    reply.code(404).send(null)
+  } else {
+    reply
+      .code(result.statusCode)
+      .type('text/html')
+      .send(result.renderResult)
+  }
+})
 
-  await app.ready()
-  const url = await app.listen(port, '0.0.0.0')
+await app.ready()
+const url = await app.listen(port, '0.0.0.0')
 
-  console.log(`Server ready and listening at ${url}`)
-}
-
-main()
+console.log(`Server ready and listening at ${url}`)
