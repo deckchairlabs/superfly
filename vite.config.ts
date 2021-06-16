7cf513943b88f1d7614a71a5f3bbd6e3a2cf1407
James Edmonds <james@deckchairlabs.com>
2021-06-16 19:49:38 +0930

    enable production sourcemaps

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
-import reactRefresh from "@vitejs/plugin-react-refresh";
-import reactJsx from "vite-react-jsx";
-import mdx from "vite-plugin-mdx";
-import ssr from "vite-plugin-ssr/plugin";
-import { UserConfig } from "vite";
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import reactJsx from 'vite-react-jsx'
+import mdx from 'vite-plugin-mdx'
+import ssr from 'vite-plugin-ssr/plugin'
+import { UserConfig } from 'vite'
 
 const config: UserConfig = {
   plugins: [reactRefresh(), reactJsx(), mdx(), ssr()],
-  optimizeDeps: { include: ["@mdx-js/react"] },
+  optimizeDeps: { include: ['@mdx-js/react'] },
   clearScreen: false,
-};
+  build: {
+    sourcemap: true,
+  },
+}
 
-export default config;
+export default config
