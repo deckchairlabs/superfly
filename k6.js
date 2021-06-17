953dfc6bd612633a3a8d4e95f4577ff68ce993ca
James Edmonds <james@deckchairlabs.com>
2021-06-17 17:00:30 +0930

    added load test

--- k6.js
+++ k6.js
@@ -1,12 +1,20 @@
+import { check, sleep } from 'k6'
 import http from 'k6/http'
-import { sleep } from 'k6'
 
-export let options = {
+export const options = {
   vus: 10,
   duration: '30s',
+  thresholds: {
+    http_req_duration: ['p(95)<500'],
+  },
 }
 
 export default function () {
-  http.get('https://deckchair-test.web.app')
+  const response = http.get(
+    `https://${__ENV.SERVICE_HOSTNAME || 'localhost:3000'}/`
+  )
+  check(response, {
+    'status is 200': (r) => r.status === 200,
+  })
   sleep(1)
 }
