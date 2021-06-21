3d9518d43efccf4e8b1d14a6a8951bdc561382fa
James Edmonds <james@deckchairlabs.com>
2021-06-21 15:02:39 +0930

    wip: Meta component

--- packages/superfly/src/types.ts
+++ packages/superfly/src/types.ts
@@ -1,9 +1,37 @@
 import { Response } from 'node-fetch'
+import { SuperflyRenderContext } from '@deckchairlabs/fastify-superfly'
 
-export type RenderResolver = (context: any) => Response
+export type RenderFunction = (context: SuperflyRenderContext) => Response
 
-export type LinksResolver = () => any[]
+export type LinksFunction = () => LinkProps[]
 
-export type MetaResolver = () => any
+export type LinkProps = {}
 
-export type LoaderResolver = () => any
+export type MetaFunction = () => MetaProps
+
+export type MetaProps = {
+  /**
+   * The document title
+   */
+  title?: string
+  /**
+   * The document description
+   */
+  description?: string
+  /**
+   * The document charset
+   * @default "utf-8"
+   */
+  charset?: string
+  /**
+   * The document viewport
+   * @default "width=device-width"
+   */
+  viewport?: string
+
+  [x: string]: string | undefined
+}
+
+export type LoaderFunction<
+  LoaderData extends object = Record<string, any>
+> = () => Promise<LoaderData> | LoaderData
